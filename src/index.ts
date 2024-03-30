ff7237fbeb29da020e0ff06991cb11c7bcd66030
mateusbrbza <mateus.c.barboza@gmail.com>
2024-03-29 23:00:44 -0300

    finished creation of a in memory database with only ts

--- src/index.ts
+++ src/index.ts
@@ -17,3 +17,18 @@ class InMemoryDB<T extends BaseRecord> implements Database<T> {
         return this.db[id]
     }
 }
+
+interface Pokemon {
+    id: string;
+    attack: number;
+    defense: number;
+}
+
+const pokemonDB = new InMemoryDB<Pokemon>();
+pokemonDB.set({
+    id: 'Bulbasaur',
+    attack: 50,
+    defense: 12,
+});
+
+console.log(pokemonDB.get('Bulbasaur'));
